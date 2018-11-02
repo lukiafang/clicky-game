7f62316264b02173e25456b90b42dd3bd13dd64b
lukia <xhumen@msn.com>
2018-11-02 00:09:42 -0400

    Updates

---
+++ precache-manifest.ca2bd19c64cdb124da0fdce657162994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "c9523501ae3941ed4172",
+    "url": "/clicky-game/static/js/main.c9523501.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/clicky-game/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "c9523501ae3941ed4172",
+    "url": "/clicky-game/static/css/main.440187f0.chunk.css"
+  },
+  {
+    "revision": "ac07a037a08ede12f10b136f4d2ab655",
+    "url": "/clicky-game/index.html"
+  }
+];
